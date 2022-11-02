210de0ccc0148d237fcdf3a91a114d0317e3b215
Анатолий Беседин <tolik777-87@mail.ru>
2022-11-02 15:59:24 +0400

    кум 0.2

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,3 +1,17 @@
 from rest_framework import serializers
 
 from reviews.models import Category, Genre, Title
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
