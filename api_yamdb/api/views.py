37177c514a54fc20ae453e3aca3c4ecea1ce37b2
Максим Валягин <deadbeat321@gmail.com>
2022-11-06 22:05:25 +0300

    Users v1.3. Возможность для уже созданных пользователей получать confirmation_code подружил с тестами.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -23,15 +23,17 @@ class RegistrationAPI(APIView):
     def post(self, request):
         data = request.data
         serializer = RegistrationSerializer(data=data)
-        username = request.data['username']
-        if User.objects.filter(username=username).exists():
-            send_confirmation_code(**data)
-            return Response(data, status=status.HTTP_200_OK)
-
-        if serializer.is_valid(raise_exception=True):
+        if serializer.is_valid(raise_exception=False):
             serializer.save()
             send_confirmation_code(**serializer.data)
             return Response(serializer.data, status=status.HTTP_200_OK)
+        username = request.data.get('username')
+        email = request.data.get('email')
+        if User.objects.filter(username=username, email=email).exists():
+            send_confirmation_code(**data)
+            return Response(
+                {'message': 'Check your email for confirmation code'},
+                status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
