dfbc37cf6539515cdcfc3b349ae04a97b11e9150
Максим Валягин <deadbeat321@gmail.com>
2022-11-08 20:09:58 +0300

    Исправил пермишн

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,12 +5,14 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated and (
-                    request.user.is_admin or request.user.is_superuser)))
+                    request.user.role == 'admin' or request.user.is_superuser)
+                    ))
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated and (
-                    request.user.is_admin or request.user.is_superuser)))
+                    request.user.role == 'admin' or request.user.is_superuser)
+                    ))
 
 
 class IsAdminPermission(permissions.BasePermission):
