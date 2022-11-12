df7efc89224010cccd906d550adb0ba9a562f8a0
Maxim Valyagin <34674231+lambazta@users.noreply.github.com>
2022-11-12 13:57:51 +0300

    Update utils.py

--- api_yamdb/users/utils.py
+++ api_yamdb/users/utils.py
@@ -1,5 +1,6 @@
 import random
 
+from django.conf import settings.EMAIL_HOST_USER
 from django.core.mail import send_mail
 
 from .models import User
@@ -9,7 +10,7 @@ def send_confirmation_code(username, email):
     subject = 'Your account confirmation code'
     confirmation_code = User.code_gen
     message = f'Your confirmation code is {confirmation_code} '
-    send_mail(subject, message, 'from@example.com', [email])
+    send_mail(subject, message, EMAIL_HOST_USER, [email])
     user_obj = User.objects.get(username=username)
     user_obj.confirmation_code = confirmation_code
     user_obj.save()
